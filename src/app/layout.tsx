5d56f1a515339d07562a9e989ef270361929f4da
Pietro <pietromizael@gmail.com>
2025-11-16 22:40:52 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Criado com a Lasy",
-  description: "Projeto criado com Lasy AI",
+  title: "Devaneios",
+  description: "Filosofias para você, guerreiro.",
 };
 
 export default function RootLayout({
